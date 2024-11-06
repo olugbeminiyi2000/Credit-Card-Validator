9f6711074d0ad4d85cdad20c1243b332a4a3f1f0
OBOLO EMMANUEL OLUWAPELUMI <96441399+olugbeminiyi2000@users.noreply.github.com>
2024-11-06 20:48:47 +0000

    Update response2.c

--- response2.c
+++ response2.c
@@ -1,8 +1,6 @@
 import os
-import socket
-import sys
 
-def get_document(root_path, location):
+def getDocument(root_path, location=""):
     search_file = os.path.join(root_path, location)
     try:
         with open(search_file, mode="r", encoding="utf-8") as fp:
@@ -13,20 +11,18 @@ def get_document(root_path, location):
             return data
     except FileNotFoundError:
         default_file = "python.txt"
-        default_data = get_document(root_path, default_file)
+        default_data = getDocument(root_path, default_file)
         return default_data
-    except Exception as exc:
-        print("Error:\n")
-        print(exc)
-        return "Error occurred while retrieving the document."
 
 def webserver():
-    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # create a phone
+    serversocket = socket(AF_INET, SOCK_STREAM)
+    print(sys.argv)
     root_path = sys.argv[1]
     try:
         serversocket.bind(("localhost", 9000))
         serversocket.listen(5)
-        while True:
+        while (1):
             (clientsocket, address) = serversocket.accept()
             rd = clientsocket.recv(5000).decode()
             pieces = rd.split("\n")
@@ -34,15 +30,14 @@ def webserver():
                 print(pieces[0])
             rq = pieces[0].split()
             location = rq[1].split("//", maxsplit=1)[-1]
-            location = location.lstrip("/")
-            data = get_document(root_path, location)
+            print(location)
+            data = getDocument(root_path, location)
             clientsocket.sendall(data.encode())
-            clientsocket.shutdown(socket.SHUT_WR)
+            clientsocket.shutdown(SHUT_WR)
+
     except KeyboardInterrupt:
         print("\nShutting down...\n")
     except Exception as exc:
         print("Error:\n")
         print(exc)
     serversocket.close()
-
-webserver()
