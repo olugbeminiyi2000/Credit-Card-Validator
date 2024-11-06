efcd9d2a2aac0ac1ba7f6b9925f04e0ba672eb88
OBOLO EMMANUEL OLUWAPELUMI <96441399+olugbeminiyi2000@users.noreply.github.com>
2024-11-06 21:01:07 +0000

    Update response1.c

--- response1.c
+++ response1.c
@@ -1,3 +1,55 @@
+import os
+import socket
+import sys
+from urllib.parse import urlparse
+
+def get_document(root_path, location):
+    file_path = os.path.join(root_path, location.lstrip('/'))
+    try:
+        with open(file_path, mode="r", encoding="utf-8") as fp:
+            data = ""
+            for line in fp:
+                line += "\r\n"
+                data += line
+            return data
+    except FileNotFoundError:
+        return None
+
+def webserver():
+    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print(sys.argv)
+    root_path = sys.argv[1]
+    try:
+        serversocket.bind(("localhost", 9000))
+        serversocket.listen(5)
+        while True:
+            (clientsocket, address) = serversocket.accept()
+            rd = clientsocket.recv(5000).decode()
+            pieces = rd.split("\n")
+            if len(pieces) > 0:
+                print(pieces[0])
+            rq = pieces[0].split()
+            parsed_url = urlparse(rq[1])
+            location = parsed_url.path
+            data = get_document(root_path, location)
+            if data is None:
+                response = "HTTP/1.1 404 Not Found\r\n\r\n"
+            else:
+                response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\n" + data
+            clientsocket.sendall(response.encode())
+            clientsocket.shutdown(socket.SHUT_WR)
+
+    except KeyboardInterrupt:
+        print("\nShutting down...\n")
+    except Exception as exc:
+        print("Error:\n")
+        print(exc)
+    serversocket.close()
+
+if __name__ == "__main__":
+    webserver()
+
+-------------------------------------------------------------------------
 import socket
 
 def webbrowser():
@@ -13,59 +65,5 @@ def webbrowser():
         print(data.decode(), end='')
     mysock.close()
 
-webbrowser()
-
-------------------------------------------------------------------------------------
-import os
-import socket
-import sys
-
-def get_document(root_path, location):
-    search_file = os.path.join(root_path, location)
-    try:
-        with open(search_file, mode="r", encoding="utf-8") as fp:
-            data = ""
-            for line in fp:
-                line += "\r\n"
-                data += line
-            return data
-    except FileNotFoundError:
-        default_file = "python.txt"
-        default_path = os.path.join(root_path, default_file)
-        if os.path.exists(default_path):
-            with open(default_path, mode="r", encoding="utf-8") as efp:
-                error_data = ""
-                for line in efp:
-                    line += "\r\n"
-                    error_data += line
-                return error_data
-        else:
-            return "Default file not found"
-
-def webserver():
-    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    root_path = sys.argv[1]
-    try:
-        serversocket.bind(("localhost", 9000))
-        serversocket.listen(5)
-        while True:
-            (clientsocket, address) = serversocket.accept()
-            rd = clientsocket.recv(5000).decode()
-            pieces = rd.split("\n")
-            if len(pieces) > 0:
-                print(pieces[0])
-            rq = pieces[0].split()
-            location = rq[1].split("//", maxsplit=1)[-1]
-            location = location.lstrip('/')
-            print(location)
-            data = get_document(root_path, location)
-            clientsocket.sendall(data.encode())
-            clientsocket.shutdown(socket.SHUT_WR)
-    except KeyboardInterrupt:
-        print("\nShutting down...\n")
-    except Exception as exc:
-        print("Error:\n")
-        print(exc)
-    serversocket.close()
-
-webserver()
+if __name__ == "__main__":
+    webbrowser()
