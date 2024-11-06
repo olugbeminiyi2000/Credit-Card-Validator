115a1cecb3572d39130ffa8b26369a6a8aa5f989
OBOLO EMMANUEL OLUWAPELUMI <96441399+olugbeminiyi2000@users.noreply.github.com>
2024-11-06 20:34:07 +0000

    Update response1.c

--- response1.c
+++ response1.c
@@ -1,77 +1,71 @@
-"""webbrowser function"""
-if __name__ == "__main__":
-    import socket
+import socket
 
-    def webbrowser():
-        mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        mysock.connect(('localhost', 9000))
-        cmd = "GET /django/django.txt HTTP/1.0\r\n\r\n".encode()
-        mysock.send(cmd)
+def webbrowser():
+    mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    mysock.connect(('localhost', 9000))
+    cmd = "GET http://127.0.0.1/django/django.txt HTTP/1.0\r\n\r\n".encode()
+    mysock.send(cmd)
 
-        while True:
-            data = mysock.recv(512)
-            if len(data) < 1:
-                break
-            print(data.decode(), end='')
-        mysock.close()
+    while True:
+        data = mysock.recv(512)
+        if len(data) < 1:
+            break
+        print(data.decode(), end='')
+    mysock.close()
 
-    webbrowser()
+webbrowser()
 
 ------------------------------------------------------------------------------------
-"""webserver"""
-if __name__ == "__main__":
-    import platform
-    from socket import *
-    import sys
-    import os
+import os
+import socket
+import sys
 
-    def get_document(root_path, location):
-        search_file = os.path.join(root_path, location)
-        try:
-            with open(search_file, mode="r", encoding="utf-8") as fp:
-                data = ""
-                for line in fp:
+def get_document(root_path, location):
+    search_file = os.path.join(root_path, location)
+    try:
+        with open(search_file, mode="r", encoding="utf-8") as fp:
+            data = ""
+            for line in fp:
+                line += "\r\n"
+                data += line
+            return data
+    except FileNotFoundError:
+        default_file = "python.txt"
+        default_path = os.path.join(root_path, default_file)
+        if os.path.exists(default_path):
+            with open(default_path, mode="r", encoding="utf-8") as efp:
+                error_data = ""
+                for line in efp:
                     line += "\r\n"
-                    data += line
-                return data
-        except FileNotFoundError:
-            default_file = "python.txt"
-            default_file_path = os.path.join(root_path, default_file)
-            try:
-                with open(default_file_path, mode="r", encoding="utf-8") as efp:
-                    error_data = ""
-                    for line in efp:
-                        line += "\r\n"
-                        error_data += line
-                    return error_data
-            except FileNotFoundError:
-                return "Default file not found"
+                    error_data += line
+                return error_data
+        else:
+            return "Default file not found"
 
-    def webserver():
-        serversocket = socket(AF_INET, SOCK_STREAM)
-        print(sys.argv)
-        root_path = sys.argv[1]
-        try:
-            serversocket.bind(("localhost", 9000))
-            serversocket.listen(5)
-            while (1):
-                (clientsocket, address) = serversocket.accept()
-                rd = clientsocket.recv(5000).decode()
-                pieces = rd.split("\n")
-                if len(pieces) > 0:
-                    print(pieces[0])
-                rq = pieces[0].split()
-                location = rq[1]
-                print(location)
-                data = get_document(root_path, location)
-                clientsocket.sendall(data.encode())
-                clientsocket.shutdown(SHUT_WR)
+def webserver():
+    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    root_path = sys.argv[1]
+    try:
+        serversocket.bind(("localhost", 9000))
+        serversocket.listen(5)
+        while True:
+            (clientsocket, address) = serversocket.accept()
+            rd = clientsocket.recv(5000).decode()
+            pieces = rd.split("\n")
+            if len(pieces) > 0:
+                print(pieces[0])
+            rq = pieces[0].split()
+            location = rq[1].split("//", maxsplit=1)[-1]
+            location = location.lstrip('/')
+            print(location)
+            data = get_document(root_path, location)
+            clientsocket.sendall(data.encode())
+            clientsocket.shutdown(socket.SHUT_WR)
+    except KeyboardInterrupt:
+        print("\nShutting down...\n")
+    except Exception as exc:
+        print("Error:\n")
+        print(exc)
+    serversocket.close()
 
-        except KeyboardInterrupt:
-            print("\nShutting down...\n")
-        except Exception as exc:
-            print("Error:\n")
-            print(exc)
-        serversocket.close()
-
-    webserver()
+webserver()
